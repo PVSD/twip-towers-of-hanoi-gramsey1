0ac6fc3b267103a3131493e18095627165c77d5b
gramsey1 <graceramsey312@yahoo.com>
2019-01-14 11:20:41 -0500

    lalalalalaalalalalal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner hi = new Scanner(System.in);
+        System.out.println("Tower of Hanoi Game.\nThis program will solve this puzzle for you.\nPlease input number of disks to solve for: ");
+        int control = hi.nextInt();
+
     }
 }
---
+++ src/com/company/TowersOfHanoi.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by gr236 on 1/14/19.
+ */
+public class TowersOfHanoi {
+    public TowersOfHanoi(int disks){
+
+    }
+}
