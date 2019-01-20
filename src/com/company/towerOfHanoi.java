7f1309526a72d7ddb38cd7bbd4e4103975b58758
gramsey1 <graceramsey312@yahoo.com>
2019-01-20 15:35:23 -0500

    i did it yeet

--- src/com/company/towerOfHanoi.java
+++ src/com/company/towerOfHanoi.java
@@ -1,13 +1,13 @@
 package com.company;
 
 public class towerOfHanoi {
-    static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
+    static void towerOfHanoi(int n, char from_tower, char to_tower, char aux_tower) {
         if (n == 1) {
-            System.out.println("Move disk 1 from rod " + from_rod + " to rod " + to_rod);
+            System.out.println("Move disk 1 from tower " + from_tower + " to " + to_tower);
             return;
         }
-        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
-        System.out.println("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod);
-        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+        towerOfHanoi(n - 1, from_tower, aux_tower, to_tower);
+        System.out.println("Move disk " + n + " from tower " + from_tower + " to " + to_tower);
+        towerOfHanoi(n - 1, aux_tower, to_tower, from_tower);
     }
 }
