6d67419e08649d9bf9f7b67047e9385e6621eacf
gramsey1 <graceramsey312@yahoo.com>
2019-01-22 11:12:44 -0500

    lalalalalaalalalalal

--- src/com/company/towerOfHanoi.java
+++ src/com/company/towerOfHanoi.java
@@ -1,13 +1,13 @@
 package com.company;
 
 public class towerOfHanoi {
-    static void towerOfHanoi(int n, int from_tower, int to_tower, int ok_tower) {
+    static void towerOfHanoi(int n, int from_tower, int to_tower, int mid_tower) {
         if (n == 1) {
             System.out.println("Move disk 1 from tower " + from_tower + " to " + to_tower);
         } else {
-            towerOfHanoi(n - 1, from_tower, ok_tower, to_tower);
+            towerOfHanoi(n - 1, from_tower, mid_tower, to_tower);
             System.out.println("Move disk " + n + " from tower " + from_tower + " to " + to_tower);
-            towerOfHanoi(n - 1, ok_tower, from_tower, to_tower);
+            towerOfHanoi(n - 1, mid_tower, to_tower, from_tower);
         }
     }
 }
