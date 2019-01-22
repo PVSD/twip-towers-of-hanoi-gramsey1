31c8239e0c0e17b17bb1bb84fabaede329882136
gramsey1 <41630887+gramsey1@users.noreply.github.com>
2019-01-22 10:49:28 -0500

    Update towerOfHanoi.java

--- src/com/company/towerOfHanoi.java
+++ src/com/company/towerOfHanoi.java
@@ -1,13 +1,13 @@
 package com.company;
 
 public class towerOfHanoi {
-    static void towerOfHanoi(int n, char from_tower, char to_tower, char aux_tower) {
+    static void towerOfHanoi(int n, char from_tower, char to_tower, char ok_tower) {
         if (n == 1) {
             System.out.println("Move disk 1 from tower " + from_tower + " to " + to_tower);
             return;
         }
-        towerOfHanoi(n - 1, from_tower, aux_tower, to_tower);
+        towerOfHanoi(n - 1, from_tower, ok_tower, to_tower);
         System.out.println("Move disk " + n + " from tower " + from_tower + " to " + to_tower);
-        towerOfHanoi(n - 1, aux_tower, to_tower, from_tower);
+        towerOfHanoi(n - 1, ok_tower, to_tower, from_tower);
     }
 }
